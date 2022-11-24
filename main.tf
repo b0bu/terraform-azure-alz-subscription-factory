e3e7964f76fdd3a5bfbe55173a50eaf6739f5441
Jason Leitch <jasonleitch0@gmail.com>
2022-11-24 20:29:33 +0000

    chore: add subscriptions

--- main.tf
+++ main.tf
@@ -24,7 +24,6 @@ module "alz-connectivity-subscription-001" {
   subscription_workload      = "Production"
 
   // assuming mg hierarchy exists individual subscirptions can be moved to the appropriate mg
-  #   # management group association variables
   #   subscription_management_group_association_enabled = true
   #   subscription_management_group_id                  = "Connectivity"
 
@@ -34,6 +33,120 @@ module "alz-connectivity-subscription-001" {
   providers = {
     azurerm = azurerm
   }
-
 }
 
+module "alz-identity-subscription-001" {
+  source   = "Azure/lz-vending/azurerm"
+  version  = "2.1.0"
+  location = "uksouth"
+
+  subscription_alias_enabled = true
+  subscription_billing_scope = data.azurerm_billing_mca_account_scope.mca.id
+  subscription_display_name  = "alz-identity-subscription-001"
+  subscription_alias_name    = "identity-001"
+  subscription_workload      = "Production"
+
+  // assuming mg hierarchy exists individual subscirptions can be moved to the appropriate mg
+  #   subscription_management_group_association_enabled = true
+  #   subscription_management_group_id                  = "Identity"
+
+  virtual_network_enabled = false
+  role_assignment_enabled = false
+
+  providers = {
+    azurerm = azurerm
+  }
+}
+
+module "alz-management-subscription-001" {
+  source   = "Azure/lz-vending/azurerm"
+  version  = "2.1.0"
+  location = "uksouth"
+
+  subscription_alias_enabled = true
+  subscription_billing_scope = data.azurerm_billing_mca_account_scope.mca.id
+  subscription_display_name  = "alz-management-subscription-001"
+  subscription_alias_name    = "management-001"
+  subscription_workload      = "Production"
+
+  // assuming mg hierarchy exists individual subscirptions can be moved to the appropriate mg
+  #   subscription_management_group_association_enabled = true
+  #   subscription_management_group_id                  = "Management"
+
+  virtual_network_enabled = false
+  role_assignment_enabled = false
+
+  providers = {
+    azurerm = azurerm
+  }
+}
+
+module "alz-sandbox-subscription-001" {
+  source   = "Azure/lz-vending/azurerm"
+  version  = "2.1.0"
+  location = "uksouth"
+
+  subscription_alias_enabled = true
+  subscription_billing_scope = data.azurerm_billing_mca_account_scope.mca.id
+  subscription_display_name  = "alz-sandbox-subscription-001"
+  subscription_alias_name    = "sandbox-001"
+  subscription_workload      = "Production"
+
+  // assuming mg hierarchy exists individual subscirptions can be moved to the appropriate mg
+  #   subscription_management_group_association_enabled = true
+  #   subscription_management_group_id                  = "Sandbox"
+
+  virtual_network_enabled = false
+  role_assignment_enabled = false
+
+  providers = {
+    azurerm = azurerm
+  }
+}
+
+module "alz-corp-subscription-001" {
+  source   = "Azure/lz-vending/azurerm"
+  version  = "2.1.0"
+  location = "uksouth"
+
+  subscription_alias_enabled = true
+  subscription_billing_scope = data.azurerm_billing_mca_account_scope.mca.id
+  subscription_display_name  = "alz-corp-subscription-001"
+  subscription_alias_name    = "corp-001"
+  subscription_workload      = "Production"
+
+  // assuming mg hierarchy exists individual subscirptions can be moved to the appropriate mg
+  #   subscription_management_group_association_enabled = true
+  #   subscription_management_group_id                  = "Corp"
+
+  virtual_network_enabled = false
+  role_assignment_enabled = false
+
+  providers = {
+    azurerm = azurerm
+  }
+}
+
+
+module "alz-corp-subscription-002" {
+  source   = "Azure/lz-vending/azurerm"
+  version  = "2.1.0"
+  location = "ukwest"
+
+  subscription_alias_enabled = true
+  subscription_billing_scope = data.azurerm_billing_mca_account_scope.mca.id
+  subscription_display_name  = "alz-corp-subscription-001"
+  subscription_alias_name    = "corp-002"
+  subscription_workload      = "Production"
+
+  // assuming mg hierarchy exists individual subscirptions can be moved to the appropriate mg
+  #   subscription_management_group_association_enabled = true
+  #   subscription_management_group_id                  = "Corp"
+
+  virtual_network_enabled = false
+  role_assignment_enabled = false
+
+  providers = {
+    azurerm = azurerm
+  }
+}
